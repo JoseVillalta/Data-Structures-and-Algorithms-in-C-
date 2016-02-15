0cd760b38881c24b8314f330fda334892bc35b14
jvillalt <jvillalta@impinj.com>
2016-02-15 08:18:37 -0800

    Performance Test

--- AlgorithmsInC++/AlgorithmsInC++.cpp
+++ AlgorithmsInC++/AlgorithmsInC++.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include "LinearSearchTests.h"
 #include "BinarySearchTest.h"
+#include "SearchPerformanceTest.h"
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
@@ -12,8 +13,10 @@ int _tmain(int argc, _TCHAR* argv[])
 
     auto lst = new LinearSearchTest();
     auto bst = new BinarySearchTest();
+    auto spt = new SearchPerformanceTest();
     lst->DoTests();
     bst->DoTest();
+    spt->DoTests();
     return 0;
 }
 
--- AlgorithmsInC++/SearchPerformanceTest.cpp
+++ AlgorithmsInC++/SearchPerformanceTest.cpp
@@ -1,7 +1,9 @@
 #include <vector>
+#include <iostream>
 #include "SearchPerformanceTest.h"
 #include "LinearSearch.h"
 #include "BinarySearch.h"
+#include "Windows.h"
 
 using namespace std;
 
@@ -10,5 +12,42 @@ void SearchPerformanceTest::DoTests()
     auto linearSearcher = new LinearSearch();
     auto binarySearcher = new BinarySearch();
 
+    //create vector with 1M values
+    vector<int> vector;
+    for (int i = 0; i < 1000000; i++)
+    {
+        vector.push_back(i * 2);
+    }
 
+    LARGE_INTEGER StartingTime, EndingTime, ElapsedMicroseconds;
+    LARGE_INTEGER Frequency;
+    int loc = 0;
+
+    QueryPerformanceFrequency(&Frequency);
+    QueryPerformanceCounter(&StartingTime);
+
+    auto found = linearSearcher->Search(vector, 488800, loc);
+
+    QueryPerformanceCounter(&EndingTime);
+
+    ElapsedMicroseconds.QuadPart = EndingTime.QuadPart - StartingTime.QuadPart;
+    ElapsedMicroseconds.QuadPart *= 1000000;
+    ElapsedMicroseconds.QuadPart /= Frequency.QuadPart;
+
+    cout << "Index is: "<<loc<<" Elapsed Microseconds Linear Search: " << ElapsedMicroseconds.QuadPart << endl;
+
+    loc = 0;
+    QueryPerformanceCounter(&StartingTime);
+    found = binarySearcher->Search(vector, loc, 488800, 0, 1000000);
+
+    QueryPerformanceCounter(&EndingTime);
+
+    ElapsedMicroseconds.QuadPart = EndingTime.QuadPart - StartingTime.QuadPart;
+    ElapsedMicroseconds.QuadPart *= 1000000;
+    ElapsedMicroseconds.QuadPart /= Frequency.QuadPart;
+
+    cout << "Index is: "<<loc<<" Elapsed Microseconds Binary Search: " << ElapsedMicroseconds.QuadPart << endl;
+
+
+    
 }
