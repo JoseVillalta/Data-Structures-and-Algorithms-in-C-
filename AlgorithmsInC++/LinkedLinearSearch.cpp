113d74dd0fc11e193c8041a23c80c5d834d19942
jvillalt <jvillalta@impinj.com>
2016-02-26 07:54:29 -0800

    Added Linked Self Organized List search. Missing tests

--- AlgorithmsInC++/LinkedLinearSearch.cpp
+++ AlgorithmsInC++/LinkedLinearSearch.cpp
@@ -1,3 +1,4 @@
+#include <iostream> //_ASSERT
 #include "LinkedLinearSearch.h"
 
 template <typename T>
@@ -38,6 +39,51 @@ bool LinkedLinearSearch::SearchIt(ListNode<T> * head, ListNode<T> &loc, T item)
         }
     }
     return found;
+}
+template<typename T>
+ListNode<T>* LinkedLinearSearch::SelfOrganizedListSearch(ListNode<T>* head, T item)
+{
+    ListNode<T> * loc = nullptr;
+    auto cur = head;
+    auto prev = head;
+    bool found = SelfOrganizedSearch(head, item, loc, cur, prev);
+    if (found)
+    {
+        _ASSERT(loc->data == item);
+    }
+
+    return loc;
+}
+
+template<typename T>
+bool LinkedLinearSearch::SelfOrganizedSearch(ListNode<T>* head, T item, ListNode<T>& loc, ListNode<T>* cur, ListNode<T>* prev)
+{
+    if (head == nullptr) return false;
+
+    if (cur->data == item)
+    {
+        loc = *cur;
+        //Move node to the top of the list right after the head node
+        ShiftList(head, cur, prev);
+        return true;
+    }
+    else
+    {
+        return SelfOrganizedSearch(head, item, loc, cur->next, cur);
+    }
+}
+
+template<typename T>
+void LinkedLinearSearch::ShiftList(ListNode<T>* head, ListNode<T>* cur, ListNode<T>* prev)
+{
+    _ASSERT(head != nullptr);
+    _ASSERT(cur != nullptr);
+    _ASSERT(prev != nullptr);
+
+    //Move node to the top of the list right after the head node
+    prev->next = cur->next;
+    cur->next = head->next;
+    head->next = cur;
 }
 
 template bool LinkedLinearSearch::Search(ListNode<int>* head, ListNode<int>& node, int item);
--- AlgorithmsInC++/LinkedLinearSearch.h
+++ AlgorithmsInC++/LinkedLinearSearch.h
@@ -10,5 +10,12 @@ public:
     bool Search(ListNode<T>* head, ListNode<T> & loc, T item);
     template<typename T>
     bool SearchIt(ListNode<T>*head, ListNode<T> & loc, T item);
+    template<typename T>
+    ListNode<T>* SelfOrganizedListSearch(ListNode<T>* head, T item);
+private:
+    template<typename T>
+    bool SelfOrganizedSearch(ListNode<T>* head, T item, ListNode<T>& loc, ListNode<T>* cur, ListNode<T>* prev);
+    template<typename T>
+    void ShiftList(ListNode<T>* head, ListNode<T>* cur, ListNode<T>* prev);
 };
 
