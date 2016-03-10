856dc500d6f77db18c346e8112e3eb90720a6377
jvillalt <jvillalta@impinj.com>
2016-03-10 07:58:46 -0800

    working on Array Based BST, first commit

---
+++ DataStructures/BSTANode.h
@@ -0,0 +1,18 @@
+#pragma once
+template<typename T>
+class BSTANode
+{
+public:
+    unsigned int m_left;
+    unsigned int m_right;
+    T m_data;
+    bool m_isFree;
+    BSTANode(int left = 0; int right = 0; data = 0; isFree = true)
+    {
+        m_left = left;
+        m_right = right;
+        m_data = data;
+        m_isFree = isFree;
+    }
+
+};
