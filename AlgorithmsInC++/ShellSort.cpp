4921e5f009a3e634b302a14301fa9b51d376349e
jvillalt <jvillalta@impinj.com>
2016-06-06 07:03:45 -0700

    Shell Sort

--- AlgorithmsInC++/ShellSort.cpp
+++ AlgorithmsInC++/ShellSort.cpp
@@ -5,26 +5,19 @@
 template <typename T>
 void ShellSorter::Sort(vector<T> & v, int start, int end)
 {
-    int lenght = end - start;
     auto gaps = { 701, 301, 132, 57, 23, 10, 4, 1 };
 
     for (auto gap : gaps)
     {
-        for (int i = 0; i <= end-gap && gap < end; i += gap)
+        for (int i = start; i <= end && gap <= end; i += gap)
         {
-            for (int j = i; j <= end-gap && gap <= end;  j += gap )
+            for (int j = i; j <= end && gap <= end;  j += gap )
             {
-                if ((j+gap) <= end)
+                if (v[i] > v[j])
                 {
-                    if (v[j] > v[j + gap])
-                    {
-                        swap(v, j, j + gap);
-                    }
-
+                    swap(v, i, j);
                 }
-                
             }
-           
         }
     }
 }
