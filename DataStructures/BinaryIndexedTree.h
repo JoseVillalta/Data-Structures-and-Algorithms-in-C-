081493dda30a486b2a121405408fefa15467867e
Jose Villalta <villalta.jose@gmail.com>
2018-01-03 08:08:59 -0800

    Fenwick Tree

---
+++ DataStructures/BinaryIndexedTree.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+
+using namespace std;
+
+class BinaryIndexedTree
+{
+public:
+	vector<int> tree;
+	BinaryIndexedTree(const vector<int>& Arg);
+	void Increase(int index, int delta);
+	int GetSum(int left, int right);
+	~BinaryIndexedTree();
+private:
+	
+	int Sum(int index);
+};
+
