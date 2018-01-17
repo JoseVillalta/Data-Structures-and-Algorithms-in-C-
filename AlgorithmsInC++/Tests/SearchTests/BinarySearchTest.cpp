94ceeb5fe868866f00b12a94eacb35580bef0ff4
jvillalt <jvillalta@impinj.com>
2018-01-17 07:36:22 -0800

    merge

--- AlgorithmsInC++/Tests/SearchTests/BinarySearchTest.cpp
+++ AlgorithmsInC++/Tests/SearchTests/BinarySearchTest.cpp
@@ -31,6 +31,23 @@ void BinarySearchTest::SearchInts()
     _ASSERT(found == false);
     _ASSERT(loc2 == 0);
 
+	int loc3 = 0;
+	found = m_binarySearcher->Search(v, loc3, 4, 0, 7);
+	_ASSERT(found == true);
+	_ASSERT(loc3 == 1);
+
+	int loc4 = 0;
+	found = m_binarySearcher->Search(v, loc4, 14, 0, 7);
+	_ASSERT(found == true);
+	_ASSERT(loc4 == 6);
+
+	vector<int> v2 = { 2, 4, 6, 8, 10, 12, 14 };
+	loc = 0;
+	found = m_binarySearcher->Search(v, loc, 4, 0, 6);
+	_ASSERT(found == true);
+	_ASSERT(loc = 1);
+
+
     cout << "Binary Search Test Successful" << endl;
 }
 
